1a7f743fb6ecd7ad0741c2380438e18a0a53c8c3
Pierre C <essaietachat@gmail.com>
2016-10-25 13:29:54 +0200

    respnsive
    Fond d’écran fonctionnel

--- client/search.js
+++ client/search.js
@@ -32,6 +32,18 @@ Template.searchBox.events({
   }, 200)
 });
 
+Template.home.onRendered(function() {
+  let settings = 'pjs-settings.json';
+  this.autorun(() => {
+    if (particlesJS) {
+      console.log(`loading particles.js config from "${settings}"...`)
+      /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+      particlesJS.load('particles-js', settings, function () {
+        console.log('callback - particles.js config loaded');
+      });
+    }
+  });
+});
 
 StandardLegends = new Mongo.Collection(null);
 
